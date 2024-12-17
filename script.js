f91a21ef5ee91a1d351daefb2ea9d74fec5faabf
Sherone <keynessherone@gmail.com>
2024-12-17 19:49:57 +0530

    added addto button , remove button

--- script.js
+++ script.js
@@ -1,41 +1,78 @@
-const Library=[]
+const Library = [];
 
-function Book(title,author,year){
-    this.title=title
-    this.author=author
-    this.year=year 
-
+function Book(title, author, year) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
 }
 
-function addToLibrary(book){
-
-    Library.push(book)
-    
+function addToLibrary(book) {
+  Library.push(book);
 }
 
-function displayBook(){
-    
-    const libraryEl=document.getElementById('Book-container');
-    libraryEl.innerHtml=""
+function displayBook() {
+  const libraryEl = document.getElementById("Book-container");
+  libraryEl.innerHTML = "";
 
-    Library.forEach(book => {
-
-        const bookCard=document.createElement('div');
-        bookCard.innerHTML=`<h1>Title:${book.title}</h1>
+  Library.forEach((book, index) => {
+    const bookCard = document.createElement("div");
+    bookCard.innerHTML = `<h1>Title:${book.title}</h1>
                             <p>Author:${book.author}</p>
-                            <p>Year:${book.year}</p>`
+                            <p>Year:${book.year}</p>`;
 
-        bookCard.className='book-card';
-        libraryEl.appendChild(bookCard);
-        
+    bookCard.className = "book-card";
+
+    const removeBtn = document.createElement("button");
+    removeBtn.innerText = "Remove Book";
+    removeBtn.className = "remove-btn";
+
+    libraryEl.appendChild(bookCard);
+    bookCard.appendChild(removeBtn);
+
+    removeBtn.addEventListener("click", () => {
+      Library.splice(index, 1);
+      displayBook();
     });
- 
+  });
 }
 
-let book1=new Book('Harry Potter','J.K Rowling','1999')
-let book2=new Book('One Piece',"Echiro Oda",'1997')
+let book1 = new Book("Harry Potter", "J.K Rowling", "1999");
+let book2 = new Book("One Piece", "Echiro Oda", "1997");
 
-addToLibrary(book1)
-addToLibrary(book2)
-displayBook()
- 
+addToLibrary(book1);
+addToLibrary(book2);
+displayBook();
+
+//modal logic
+
+const modelContainerEl = document.getElementById("modal-container");
+const bookformEl = document.getElementById("form-book");
+
+const addBtnEl = document.getElementById("add-book-btn");
+const closeBtnEl = document.getElementById("close-btn");
+
+//button logics
+addBtnEl.addEventListener("click", () => {
+  modelContainerEl.style.display = "flex";
+});
+
+closeBtnEl.addEventListener("click", () => {
+  modelContainerEl.style.display = "none";
+});
+
+bookformEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const year = document.getElementById("year").value;
+
+  const newBook = new Book(title, author, year);
+
+  addToLibrary(newBook);
+
+  displayBook();
+
+  modelContainerEl.style.display = "none";
+  bookformEl.reset();
+});
